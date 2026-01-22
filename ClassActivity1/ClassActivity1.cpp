949fd2f8d7fba2c5a32de757b9e070f59e192efd
jellis34 <jellis34@atu.edu>
2026-01-22 11:58:34 -0600

    Added Main Function

--- ClassActivity1/ClassActivity1.cpp
+++ ClassActivity1/ClassActivity1.cpp
@@ -46,5 +46,23 @@ class Dog {
 
 int main()
 {
-	cout << "Hello World!\n";
+	// Initializes dog1 Object:
+	Dog dog1;
+
+	// Sets Data for dog1:
+	dog1.setName("Spot");
+	dog1.setAge(11);
+	dog1.setBreed("Golden Retriever");
+
+	// Prints Data for dog1:
+	cout << "Dog 1 is named " << dog1.getName() << ", he is " << dog1.getAge() << ", and he is a " << dog1.getBreed() << "." << endl;
+
+	// Uses setter fuction to modify dog1's Age:
+	dog1.setAge(12);
+
+	// Reprints dog1's data to show the change:
+	cout << "Dog 1 is named " << dog1.getName() << ", he is " << dog1.getAge() << ", and he is a " << dog1.getBreed() << "." << endl;
+
+	// Calls the bark() function
+	dog1.bark();
 }
