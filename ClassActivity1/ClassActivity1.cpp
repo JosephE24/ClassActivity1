55a5b1e88bd7cb74fd6f02aa3ec1b3e99071e972
jellis34 <jellis34@atu.edu>
2026-01-22 12:02:58 -0600

    Made Main Function More Concise

--- ClassActivity1/ClassActivity1.cpp
+++ ClassActivity1/ClassActivity1.cpp
@@ -57,12 +57,6 @@ int main()
 	// Prints Data for dog1:
 	cout << "Dog 1 is named " << dog1.getName() << ", he is " << dog1.getAge() << ", and he is a " << dog1.getBreed() << "." << endl;
 
-	// Uses setter fuction to modify dog1's Age:
-	dog1.setAge(12);
-
-	// Reprints dog1's data to show the change:
-	cout << "Dog 1 is named " << dog1.getName() << ", he is " << dog1.getAge() << ", and he is a " << dog1.getBreed() << "." << endl;
-
 	// Calls the bark() function
 	dog1.bark();
 }
